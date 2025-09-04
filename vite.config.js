85372cac219fe8d2c857b046836c982e33913706
Adrián Litardo <lalitard@espol.edu.ec>
2025-09-04 16:34:37 -0500

    Actualizar la configuración de homepage en package.json y ajustar la base en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Museo360oficial/' // Añade esta línea
+  base: '/Museo360oficial'
 })
